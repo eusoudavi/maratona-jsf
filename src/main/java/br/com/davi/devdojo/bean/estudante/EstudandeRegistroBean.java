eb63d9f550d8a4d855d7454a187f69cb32d15956
davi.visintainer <davi.visintainer@placeti.com.br>
2022-10-21 13:00:01 -0300

    Aula 07 - Coleções

--- src/main/java/br/com/davi/devdojo/bean/estudante/EstudandeRegistroBean.java
+++ src/main/java/br/com/davi/devdojo/bean/estudante/EstudandeRegistroBean.java
@@ -4,12 +4,50 @@ import br.com.davi.devdojo.model.Estudante;
 
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.*;
+
+import static java.util.Arrays.asList;
 
 @Named("estudanteRegistro")     // ALTERANDO O NOME AQUI, DEVEMOS CHAMAR POR ESSE NOME NO JSF.
 public class EstudandeRegistroBean implements Serializable {
 //    CASO NÃO ALTERÁSSEMOS O NOME, CHAMARIAMOS POR estudanteRegistroBean, NO JSF.
 
     private Estudante estudante = new Estudante();
+    private String[] nomesArray = {"Davi", "Menezes", "Visintainer"};
+    private List<String> nomesList = asList("10", "20", "30");
+    private Set<String> nomesSet = new HashSet<>(asList("A", "B", "C"));
+    private Map<String, String> nomesMap = new HashMap<>();
+
+    {
+        nomesMap.put("Davi", "Primeiro Nome");
+        nomesMap.put("Menezes", "Segundo Nome");
+        nomesMap.put("Visintainer", "Último Nome");
+    }
+
+
+    public Map<String, String> getNomesMap() {
+        return nomesMap;
+    }
+
+    public void setNomesMap(Map<String, String> nomesMap) {
+        this.nomesMap = nomesMap;
+    }
+
+    public Set<String> getNomesSet() {
+        return nomesSet;
+    }
+
+    public void setNomesSet(Set<String> nomesSet) {
+        this.nomesSet = nomesSet;
+    }
+
+    public List<String> getNomesList() {
+        return nomesList;
+    }
+
+    public void setNomesList(List<String> nomesList) {
+        this.nomesList = nomesList;
+    }
 
     public Estudante getEstudante() {
         return estudante;
@@ -17,5 +55,13 @@ public class EstudandeRegistroBean implements Serializable {
 
     public void setEstudante(Estudante estudante) {
         this.estudante = estudante;
+    }
+
+    public String[] getNomesArray() {
+        return nomesArray;
+    }
+
+    public void setNomesArray(String[] nomesArray) {
+        this.nomesArray = nomesArray;
     }
 }
