cf16c84e2d3427b1584833e0249db5b16b02b4ae
davi.visintainer <davi.visintainer@placeti.com.br>
2022-10-22 16:56:50 -0300

    Aula 10_mostrar e esconder links_

--- src/main/java/br/com/davi/devdojo/bean/estudante/EstudandeRegistroBean.java
+++ src/main/java/br/com/davi/devdojo/bean/estudante/EstudandeRegistroBean.java
@@ -2,6 +2,7 @@ package br.com.davi.devdojo.bean.estudante;
 
 import br.com.davi.devdojo.model.Estudante;
 
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.util.*;
@@ -9,6 +10,7 @@ import java.util.*;
 import static java.util.Arrays.asList;
 
 @Named("estudanteRegistro")     // ALTERANDO O NOME AQUI, DEVEMOS CHAMAR POR ESSE NOME NO JSF.
+@ViewScoped                     // ENQUANTO O USUÁRIO ESTÁ NA PÁGINA, O JAVA TEM QUE MANDATER O ESTADO DAS VARIÁVEIS
 public class EstudandeRegistroBean implements Serializable {
 //    CASO NÃO ALTERÁSSEMOS O NOME, CHAMARIAMOS POR estudanteRegistroBean, NO JSF.
 
@@ -17,12 +19,13 @@ public class EstudandeRegistroBean implements Serializable {
     private List<String> nomesList = asList("10", "20", "30");
     private Set<String> nomesSet = new HashSet<>(asList("A", "B", "C"));
     private Map<String, String> nomesMap = new HashMap<>();
-
     {
         nomesMap.put("Davi", "Primeiro Nome");
         nomesMap.put("Menezes", "Segundo Nome");
         nomesMap.put("Visintainer", "Último Nome");
     }
+    private Boolean mostrarNotas;
+    private Boolean mostrarLink;
 
     public void executar(String nome){
         System.out.println("Fazendo uma busca no BD do nome " + nome);
@@ -35,6 +38,37 @@ public class EstudandeRegistroBean implements Serializable {
    public String irParaIndex2(){
 //        return "index2";
         return "index2?faces-redirect=true"; // USAMOS ESSA ESTRUTURA PARA MOSTRAR NO NAVEGADOR O NOVO ENDEREÇO
+    }
+
+    public void exibirNotas(){
+        this.mostrarNotas = true;
+    }
+    public void esconderNotas(){
+        this.mostrarNotas = false;
+    }
+
+    public void exibirLink(){
+        this.mostrarLink = true;
+    }
+
+    public void esconderLink(){
+        this.mostrarLink = false;
+    }
+
+
+    public Boolean getMostrarLink() {
+        return mostrarLink;
+    }
+
+    public void setMostrarLink(Boolean mostrarLink) {
+        this.mostrarLink = mostrarLink;
+    }
+
+    public Boolean getMostrarNotas() {
+        return mostrarNotas;
+    }
+    public void setMostrarNotas(Boolean mostrarNotas) {
+        this.mostrarNotas = mostrarNotas;
     }
 
     public Map<String, String> getNomesMap() {
